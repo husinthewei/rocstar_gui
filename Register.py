e2a0a868f8169cdc78f11cf540c187970d97614c
Weiwei Hu <wae3wae@yahoo.com>
2019-10-02 18:23:44 -0400

    Working register communication with dummy server and working formatting

--- Register.py
+++ Register.py
@@ -1,5 +1,74 @@
-class Register:
-	def __init__(self):
-		print("Hello world")
+from SmtpBoard import SmtpBoard
 
-Register()
+class Register:
+    def __init__(self,
+                 addr,
+                 smtp_board,
+                 name = "unnamed",
+                 v5 = False,
+                 format_method = None,
+                 refresh_rate = 1):
+        self.name = name
+        self.addr = addr
+        self.board = smtp_board
+        if v5:
+            self.read_method = Register.read_s6
+            self.write_method = Register.write_s6
+        else:
+            self.read_method = Register.read_uzed
+            self.write_method = Register.write_uzed
+        self.fmt = format_method if format_method else Register.format_04x
+        self.data = -1
+
+    def write_s6(self, data):
+        addr = self.addr | 0x10000
+        return self.board.wr(addr, data)
+
+    def write_uzed(self, data):
+        return self.board.wr(self.addr, data)
+
+    def read_s6(self):
+        addr = self.addr | 0x10000
+        self.data = self.board.rd(addr)
+        return self.data
+
+    def read_uzed(self):
+        self.data = self.board.rd(self.addr)
+        return self.data
+
+    def rd(self):
+        return self.read_method(self)
+
+    def wr(self, data):
+        return self.write_method(self, data)
+
+    def rfmt(self):
+        return self.fmt(self)
+
+    def format_d(self):
+        #format data as decimal int (%d)
+        return "%d"%(self.data)
+
+    def format_04x(self):
+        #format data as 4-digit hexadecimal int (%04x)
+        return "%04x"%(self.data)
+
+    def format_time(self):
+        # format data as time hh:mm:ss
+        if self.data<0:
+            return "??:??:??"
+        d = self.data
+        return "%02d:%02d:%02d"%(d/3600, (d%3600)/60, d%60)
+
+test_board = SmtpBoard()
+reg1 = Register(0x1000, test_board, v5 = True)
+reg2 = Register(0x1000, test_board, v5 = False, format_method = Register.format_time)
+
+print(reg1.wr(0x1))
+print(reg2.wr(0xbeef))
+
+print("reg1 %x"%(reg1.rd()))
+print("reg2 %x"%(reg2.rd()))
+
+print(reg1.rfmt())
+print(reg2.rfmt())
---
+++ SmtpBoard.py
@@ -0,0 +1,41 @@
+import socket
+
+class SmtpBoard:
+    def __init__(self, addr = "127.0.0.1"):
+        self.addr = addr
+        self.port = 31415
+
+        # use TCP for board I/O
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.addr, self.port))
+        # 10 millisecond I/O timeout for reponses from board
+        self.socket.settimeout(self.TIMEOUT_SECONDS)
+    
+    RECVBUFFERLEN = 2048
+    TIMEOUT_SECONDS = 0.010
+
+    def rd(self, addr):
+    	message = "R %x\n"%(addr)
+    	self.socket.send(message.encode())
+    	data = None
+    	try:
+    		data = self.socket.recv(self.RECVBUFFERLEN)
+    	except socket.timeout:
+    		pass
+    	data = str(data)[2:][:-3]
+    	data_pieces = data.split(" ")
+    	if data_pieces[0] == "250":
+    		return int(data_pieces[3], 16)
+    	else:
+    		return -1
+
+    # Returns data written on success, otherwise -1
+    def wr(self, addr, data):
+    	message = "W %x %x\n"%(addr, data)
+    	self.socket.send(message.encode())
+    	response = self.socket.recv(self.RECVBUFFERLEN)
+    	response_pieces = str(response)[2:][:-3].split(" ")
+    	if response_pieces[0] == "250":
+    		return int(response_pieces[3], 16)
+    	else:
+    		return -1
